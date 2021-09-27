a1ecf21dc2d6df2692557ebaeb48fcda0e9c0f24
Maria Telenczuk <maja_ka@hotmail.com>
2021-09-27 09:42:04 +0200

    Data factory and conftest (#13)
    * init data_factory file (based on the substra data_factory)
    
    * Update tests/sdk/data_factory.py
    
    Co-authored-by: Thaïs de Boisfossé <4139100+Esadruhn@users.noreply.github.com>
    
    * add setup.cfg for pytest
    
    * move conftest.py to base directory
    
    * move conftest from sdk
    
    * set debug to True for client
    
    * towards adding data factory to the test_strategies
    
    * updating data_factory to work with the connectlib settings
    
    Co-authored-by: Thaïs de Boisfossé <4139100+Esadruhn@users.noreply.github.com>

---
+++ conftest.py
@@ -0,0 +1,7 @@
+# Even if empty this file is useful so that when running from the root folder
+# ./connectlib is added to sys.path by pytest. See
+# https://docs.pytest.org/en/latest/explanation/pythonpath.html for more
+# details.
+# Eg, this allows to build extensions in place and run pytest
+# docs/contribute/mistakes.rst and use connectlib from the local folder rather
+# than the one from site-packages.
