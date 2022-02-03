74fdc394a9a587508c14dbb54293434fd018b24a
Fabien GELUS <45175277+Fabien-GELUS@users.noreply.github.com>
2022-02-03 15:21:20 +0100

    chore: Release 0.7.0 (#81)
    * release
    
    * Update CHANGELOG.md
    
    Co-authored-by: Goussault Romain <romain.goussault@owkin.com>
    
    * Update CHANGELOG.md
    
    Co-authored-by: Thaïs de Boisfossé <4139100+Esadruhn@users.noreply.github.com>
    
    * update minimum substra requirement
    
    Co-authored-by: Goussault Romain <romain.goussault@owkin.com>
    Co-authored-by: Thaïs de Boisfossé <4139100+Esadruhn@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -57,8 +57,8 @@ setup(
     install_requires=[
         "numpy>=1.20.3",
         "cloudpickle>=1.6.0",
-        "substratools>=0.9.0",
-        "substra>=0.13.0",
+        "substratools>=0.9.1",
+        "substra>=0.16.0",
         "pydantic>=1.9.0",
         "pip>=21.2",
         "wheel",
