f82dfc9172b931bb1d8c279e3ed0ed7433b03ffb
Thaïs de Boisfossé <4139100+Esadruhn@users.noreply.github.com>
2022-06-14 14:29:09 +0200

    feat: rename the OneOrganization strategy to SingleOrganization (#206)

--- connectlib/algorithms/pytorch/__init__.py
+++ connectlib/algorithms/pytorch/__init__.py
@@ -1,5 +1,5 @@
 from connectlib.algorithms.pytorch.torch_fed_avg_algo import TorchFedAvgAlgo
-from connectlib.algorithms.pytorch.torch_one_organization_algo import TorchOneOrganizationAlgo
 from connectlib.algorithms.pytorch.torch_scaffold_algo import TorchScaffoldAlgo
+from connectlib.algorithms.pytorch.torch_single_organization_algo import TorchSingleOrganizationAlgo
 
-__all__ = ["TorchFedAvgAlgo", "TorchOneOrganizationAlgo", "TorchScaffoldAlgo"]
+__all__ = ["TorchFedAvgAlgo", "TorchSingleOrganizationAlgo", "TorchScaffoldAlgo"]
