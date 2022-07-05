a3f20add8923a2382b1a059567744f5185178d24
Fabien GELUS <45175277+Fabien-GELUS@users.noreply.github.com>
2022-07-05 10:37:29 +0200

    Release 0.20.0 (#222)
    * chore: release 0.20.0
    
    * woops

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     install_requires=[
         "numpy>=1.20.3",
         "cloudpickle>=1.6.0",
-        "substra==0.27.0",
+        "substra==0.28.0",
         "substratools==0.13.0",
         "pydantic>=1.9.0",
         "pip>=21.2",
