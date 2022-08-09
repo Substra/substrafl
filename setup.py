caa4049c20ea9e03f27e3bfdc3ea8ba5de499400
Thaïs de Boisfossé <4139100+Esadruhn@users.noreply.github.com>
2022-08-09 14:14:29 +0200

    chore: add license (#256)

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-# Copyright 2018 Owkin, inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """Packaging settings."""
 import os
 from codecs import open
@@ -35,9 +21,8 @@ setup(
     description="Federated Learning library",
     long_description="",
     long_description_content_type="text/markdown",
-    url="https://owkin-substrafl.readthedocs-hosted.com/en/latest/",
-    author="Owkin",
-    author_email="flow@owkin.com",
+    url="https://connect-docs.owkin.com/",
+    author="Owkin, Inc.",
     license="Apache 2.0",
     classifiers=[
         "Intended Audience :: Developers",
