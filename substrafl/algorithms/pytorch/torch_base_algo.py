d89fcbb0162c309899dde10ed2c7c826f585c418
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2022-09-23 15:25:17 +0200

    typo
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- substrafl/algorithms/pytorch/torch_base_algo.py
+++ substrafl/algorithms/pytorch/torch_base_algo.py
@@ -109,7 +109,7 @@ class TorchAlgo(Algo):
 
         # Create torch dataset
         predict_dataset = self._dataset(datasamples, is_inference=True)
-        pred = self._local_predict(predict_dataset=predict_dataset, predictions_path=predictions_path)
+        self._local_predict(predict_dataset=predict_dataset, predictions_path=predictions_path)
 
     def _save_predictions(self, predictions: torch.Tensor, predictions_path: os.PathLike):
         """Save the predictions under the torch format.
