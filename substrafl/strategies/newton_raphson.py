927dbf2f9d81b82166b305d9f403f3761699c619
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2022-10-17 11:37:35 +0200

    evaluate at init
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- substrafl/strategies/newton_raphson.py
+++ substrafl/strategies/newton_raphson.py
@@ -97,7 +97,7 @@ class NewtonRaphson(Strategy):
         if aggregation_node is None:
             raise ValueError("In Newton-Raphson strategy aggregation node cannot be None")
 
-        if round_idx == 1:
+        if round_idx == 0:
             # Initialization of the strategy by performing a local update on each train data node
             assert self._local_states is None
             assert self._shared_states is None
@@ -110,24 +110,25 @@ class NewtonRaphson(Strategy):
                 clean_models=clean_models,
             )
 
-        current_aggregation = aggregation_node.update_states(
-            self.compute_averaged_states(
-                shared_states=self._shared_states,
-                _algo_name="Aggregating",
-            ),  # type: ignore
-            round_idx=round_idx,
-            authorized_ids=list(set([train_data_node.organization_id for train_data_node in train_data_nodes])),
-            clean_models=clean_models,
-        )
+        else:
+            current_aggregation = aggregation_node.update_states(
+                self.compute_averaged_states(
+                    shared_states=self._shared_states,
+                    _algo_name="Aggregating",
+                ),  # type: ignore
+                round_idx=round_idx,
+                authorized_ids=list(set([train_data_node.organization_id for train_data_node in train_data_nodes])),
+                clean_models=clean_models,
+            )
 
-        self._perform_local_updates(
-            algo=algo,
-            train_data_nodes=train_data_nodes,
-            current_aggregation=current_aggregation,
-            round_idx=round_idx,
-            aggregation_id=aggregation_node.organization_id,
-            clean_models=clean_models,
-        )
+            self._perform_local_updates(
+                algo=algo,
+                train_data_nodes=train_data_nodes,
+                current_aggregation=current_aggregation,
+                round_idx=round_idx,
+                aggregation_id=aggregation_node.organization_id,
+                clean_models=clean_models,
+            )
 
     @remote
     def compute_averaged_states(
