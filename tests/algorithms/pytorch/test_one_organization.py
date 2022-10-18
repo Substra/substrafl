decf169980733228388b04b09212de9db2481ba3
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2022-10-18 14:51:23 +0200

    update test
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- tests/algorithms/pytorch/test_one_organization.py
+++ tests/algorithms/pytorch/test_one_organization.py
@@ -10,7 +10,6 @@ from substrafl.evaluation_strategy import EvaluationStrategy
 from substrafl.index_generator import NpIndexGenerator
 from substrafl.model_loading import download_algo_files
 from substrafl.model_loading import load_algo
-from substrafl.nodes.node import OutputIdentifiers
 from substrafl.strategies import SingleOrganization
 from tests import utils
 
@@ -19,24 +18,15 @@ logger = logging.getLogger(__name__)
 EXPECTED_PERFORMANCE = 0.2774176577698596
 
 
-@pytest.mark.substra
-@pytest.mark.slow
-def test_one_organization(
+@pytest.fixture(scope="module")
+def compute_plan(
     network,
     torch_linear_model,
     train_linear_nodes,
     test_linear_nodes,
     session_dir,
-    test_linear_data_samples,
-    mae,
     numpy_torch_dataset,
 ):
-    """End to end test for torch one organization algorithm. Checking that the perf are the same for :
-    different combinations of n_updates and n_rounds
-     The expected result was calculated to be the same for the local mode and for the in pure Substra. For the
-     details of the implementation of the latter ones please go to PR #109
-    """
-
     # Common definition
     seed = 42
     algo_deps = Dependency(
@@ -70,7 +60,7 @@ def test_one_organization(
             )
 
     my_algo = MyOneOrganizationAlgo()
-    my_eval_strategy = EvaluationStrategy(test_data_nodes=test_linear_nodes[:1], rounds=1)  # test every round
+    my_eval_strategy = EvaluationStrategy(test_data_nodes=test_linear_nodes[:1], rounds=[0, N_ROUND])
 
     compute_plan = execute_experiment(
         client=network.clients[0],
@@ -86,19 +76,42 @@ def test_one_organization(
     # Wait for the compute plan to be finished
     utils.wait(network.clients[0], compute_plan)
 
-    tasks = network.clients[0].list_task(filters={"compute_plan_key": [compute_plan.key]})
-    testtuples = [t for t in tasks if t.outputs.get(OutputIdentifiers.performance) is not None]
-    testtuples = sorted(testtuples, key=lambda x: x.rank)
-
-    # ensure that final result is correct up to 6 decimal points
-    assert testtuples[-1].outputs[OutputIdentifiers.performance].value == pytest.approx(EXPECTED_PERFORMANCE, rel=10e-6)
-
-    assert local_model_perf(network, compute_plan, session_dir, test_linear_data_samples, mae) == pytest.approx(
-        EXPECTED_PERFORMANCE
-    )
+    return compute_plan
 
 
-def local_model_perf(network, compute_plan, session_dir, test_linear_data_samples, mae):
+@pytest.mark.substra
+@pytest.mark.slow
+def test_one_organization_algo_performance(
+    network,
+    compute_plan,
+    torch_linear_model,
+    test_linear_data_samples,
+    mae,
+    rtol,
+):
+    """End to end test for torch one organization algorithm. Checking that the perf are the same for :
+    different combinations of n_updates and n_rounds
+     The expected result was calculated to be the same for the local mode and for the in pure Substra. For the
+     details of the implementation of the latter ones please go to PR #109
+    """
+
+    """End to end test for torch scaffold algorithm."""
+
+    perfs = network.clients[0].get_performances(compute_plan.key)
+    assert pytest.approx(EXPECTED_PERFORMANCE, rel=rtol) == perfs.performance[1]
+
+    seed = 42
+    torch.manual_seed(seed)
+
+    model = torch_linear_model()
+    y_pred = model(torch.from_numpy(test_linear_data_samples[0][:, :-1]).float()).detach().numpy().reshape(-1)
+    y_true = test_linear_data_samples[0][:, -1]
+
+    performance_at_init = mae.compute(y_pred, y_true)
+    assert performance_at_init == perfs.performance[0]
+
+
+def test_download_load_algo(network, compute_plan, session_dir, test_linear_data_samples, mae, rtol):
     download_algo_files(
         client=network.clients[0], compute_plan_key=compute_plan.key, round_idx=None, dest_folder=session_dir
     )
@@ -108,4 +121,4 @@ def local_model_perf(network, compute_plan, session_dir, test_linear_data_sample
     y_true = test_linear_data_samples[0][:, -1:].reshape(-1)
     performance = mae.compute(y_pred, y_true)
 
-    return performance
+    assert performance == pytest.approx(EXPECTED_PERFORMANCE, rel=rtol)
