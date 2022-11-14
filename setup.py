ff6015e50f1f4e2185b7dab9d300cae4fc3a121f
Guilhem Barthes <guilhem.barthes@owkin.com>
2022-11-14 16:46:49 +0100

    build: add python 3.10 classifier
    Signed-off-by: Guilhem Barthes <guilhem.barthes@owkin.com>

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+         "Programming Language :: Python :: 3.10",
     ],
     keywords=["substrafl"],
     packages=find_packages(exclude=["tests*", "benchmark*"]),
