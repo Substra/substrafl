4525e009101e64eefbc1da993bc80ccd74915492
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2022-12-06 13:37:15 +0100

    fork error on windows
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -47,7 +47,7 @@ def pytest_addoption(parser):
 def set_multiprocessing_variable():
     # https://github.com/pytest-dev/pytest-flask/issues/104
     # necessary on OS X to run multiprocessing
-    multiprocessing.set_start_method("fork")
+    multiprocessing.set_start_method("fork", force=True)
 
 
 @pytest.fixture(scope="session")
