8ecd414ac78b8323134542bb3fff5aead58948a6
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2022-12-12 10:38:24 +0100

    test win subprocess
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -47,7 +47,7 @@ def pytest_addoption(parser):
 def set_multiprocessing_variable():
     # https://github.com/pytest-dev/pytest-flask/issues/104
     # necessary on OS X to run multiprocessing
-    multiprocessing.set_start_method("fork")
+    multiprocessing.set_start_method("spawn")
 
 
 @pytest.fixture(scope="session")
