dec36e6ec9045812e5d6d4ceea0db33aef0c023a
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2022-12-20 14:42:59 +0100

    release
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     install_requires=[
         "numpy>=1.20.3,!=1.24.0",
         "cloudpickle>=1.6.0",
-        "substra~=0.41.0rc1",
+        "substra~=0.41.0rc2",
         "substratools~=0.20.0",
         "pydantic>=1.9.0",
         "pip>=21.2",
