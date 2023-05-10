c2de285fbb0e05438d00e7f43266c1c3da57a0f6
Guilhem Barthes <guilhem.barthes@owkin.com>
2023-05-10 14:36:51 +0200

    feat: add more file formats to exclude
    Signed-off-by: Guilhem Barthes <guilhem.barthes@owkin.com>

--- substrafl/dependency/path_management.py
+++ substrafl/dependency/path_management.py
@@ -10,19 +10,78 @@ from typing import Set
 logger = logging.getLogger(__name__)
 
 TMP_SUBSTRAFL_PREFIX = "tmp_substrafl"
+# Common paths regex we want to exclude, partially based on
+# https://github.com/github/gitignore/blob/main/Python.gitignore
 EXCLUDED_PATHS_REGEX_DEFAULT = [
+    # Byte-compiled / optimized / DLL files
+    "__pycache__/*",
+    "*.py[cod]",
+    "*$py.class",
+    "htmlcov/*",
+    ".tox/*",
+    ".nox/*",
+    ".coverage",
+    ".coverage.*",
+    ".cache",
+    "nosetests.xml",
+    "coverage.xml",
+    "*.cover",
+    "*.py,cover",
+    ".hypothesis/*",
+    ".pytest_cache/*",
+    "cover/*",
+    # Jupyter Notebook
+    ".ipynb_checkpoints",
+    # SageMath parsed files
+    "*.sage.py",
+    # Environments
+    ".env",
+    ".venv",
+    "env/*",
+    "venv/*",
+    "ENV/*",
+    "env.bak/*",
+    "venv.bak/*",
+    # Spyder project settings
+    ".spyderproject",
+    ".spyproject",
+    # Rope project settings
+    ".ropeproject",
+    # mkdocs documentation
+    "/site",
+    # mypy
+    ".mypy_cache/*",
+    ".dmypy.json",
+    "dmypy.json",
+    # Pyre type checker
+    ".pyre/*",
+    # pytype static type analyzer
+    ".pytype/*",
+    # Cython debug symbols
+    "cython_debug/*",
+    # PyCharm
+    #  JetBrains specific template is maintained in a separate JetBrains.gitignore that can
+    #  be found at https://github.com/github/gitignore/blob/main/Global/JetBrains.gitignore
+    #  and can be added to the global gitignore or merged into this file.  For a more nuclear
+    #  option (not recommended) you can uncomment the following to ignore the entire idea folder.
+    ".idea/*",
+    # VS Code
+    ".vscode/*",
+    # Custom
     # Common data extensions
     "*.csv",
-    "*.xls",
-    "*.xlsx",
-    # Python data extensiosn
-    "*.npy",
-    # Common image extensions
-    "*.png",
+    "*.hdf",
     "*.jpg",
     "*.jpeg",
+    "*.npy",
+    "*.png",
+    "*.pqt",
+    "*.parquet",
+    "*.xls",
+    "*.xlsx",
     # Common folders
     ".git/*",
+    ".github/*"
     # Others
     "local-worker*",
     TMP_SUBSTRAFL_PREFIX + "*",
