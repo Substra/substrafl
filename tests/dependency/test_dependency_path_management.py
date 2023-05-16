35f930c5208830792de82770ded3ecc54866d559
Guilhem Barthes <guilhem.barthes@owkin.com>
2023-05-16 18:08:25 +0200

    fix: test imports
    Signed-off-by: Guilhem Barthes <guilhem.barthes@owkin.com>

--- tests/dependency/test_dependency_path_management.py
+++ tests/dependency/test_dependency_path_management.py
@@ -6,8 +6,7 @@ from typing import Optional
 
 import pytest
 
-from substrafl.dependency.path_management import TMP_SUBSTRAFL_PREFIX
-from substrafl.dependency.path_management import DependencyPathManagement
+from substrafl.dependency import path_management
 
 
 @pytest.fixture
@@ -55,7 +54,7 @@ def create_random_folder(create_random_path):
 
 def test_expand_regex_file(tmp_dir, create_random_file):
     path = create_random_file(tmp_dir)
-    paths = DependencyPathManagement.expand_regexes("*", [tmp_dir])
+    paths = path_management.expand_regexes("*", [tmp_dir])
     assert paths == [path]
 
 
@@ -63,20 +62,20 @@ def test_expand_regex_folder(tmp_dir, create_random_folder, create_random_file):
     folder_path = create_random_folder(tmp_dir)
     path = create_random_file(folder_path)
 
-    paths = DependencyPathManagement.expand_regexes("*", [folder_path])
+    paths = path_management.expand_regexes("*", [folder_path])
     assert paths == [path]
 
 
 def test_expand_paths_file(tmp_dir, create_random_file):
     path = create_random_file(tmp_dir)
-    paths = DependencyPathManagement.expand_paths([tmp_dir])
+    paths = path_management.expand_paths([tmp_dir])
     assert paths == {path}
 
 
 def test_expand_paths_folders(tmp_dir, create_random_folder, create_random_file):
     folder_path = create_random_folder(tmp_dir)
     paths = {create_random_file(folder_path) for _ in range(10)}
-    found_paths = DependencyPathManagement.expand_paths([tmp_dir])
+    found_paths = path_management.expand_paths([tmp_dir])
     assert paths == found_paths
 
 
@@ -92,7 +91,7 @@ def test_get_excluded_paths_default(tmp_dir, create_random_file):
     jpeg_file = create_random_file(tmp_dir, suffix=".jpeg")
     git_file = create_random_file(tmp_dir / ".git")
     local_worker_file = create_random_file(tmp_dir / "local-worker")
-    tmp_file = create_random_file(tmp_dir / TMP_SUBSTRAFL_PREFIX)
+    tmp_file = create_random_file(tmp_dir / path_management.TMP_SUBSTRAFL_PREFIX)
 
     paths = {
         csv_file,
@@ -107,15 +106,13 @@ def test_get_excluded_paths_default(tmp_dir, create_random_file):
         local_worker_file,
         tmp_file,
     }
-    excluded_paths = DependencyPathManagement.get_excluded_paths(
-        src=[tmp_dir], excluded=[], not_excluded=[], excluded_regex=[]
-    )
+    excluded_paths = path_management.get_excluded_paths(src=[tmp_dir], excluded=[], not_excluded=[], excluded_regex=[])
     assert paths == excluded_paths
 
 
 def test_get_excluded_paths_excluded_file_absolute(tmp_dir, create_random_file):
     path_excluded = create_random_file(tmp_dir)
-    excluded_paths = DependencyPathManagement.get_excluded_paths(
+    excluded_paths = path_management.get_excluded_paths(
         src=[tmp_dir], excluded=[path_excluded], not_excluded=[], excluded_regex=[]
     )
     assert {path_excluded} == excluded_paths
@@ -124,7 +121,7 @@ def test_get_excluded_paths_excluded_file_absolute(tmp_dir, create_random_file):
 def test_get_excluded_paths_excluded_file_relative(tmp_dir, create_random_file):
     path_excluded = create_random_file(tmp_dir)
     path_excluded_relative = path_excluded.relative_to(tmp_dir.parent)
-    excluded_paths = DependencyPathManagement.get_excluded_paths(
+    excluded_paths = path_management.get_excluded_paths(
         src=[tmp_dir], excluded=[path_excluded_relative], not_excluded=[], excluded_regex=[]
     )
     assert {path_excluded} == excluded_paths
@@ -135,7 +132,7 @@ def test_get_excluded_paths_not_excluded_file_absolute(tmp_dir, create_random_fi
     xls_file = create_random_file(tmp_dir, suffix=".xls")
 
     paths = {csv_subfolder_file}
-    excluded_paths = DependencyPathManagement.get_excluded_paths(
+    excluded_paths = path_management.get_excluded_paths(
         src=[tmp_dir], excluded=[], not_excluded=[xls_file], excluded_regex=[]
     )
     assert paths == excluded_paths
@@ -146,7 +143,7 @@ def test_get_excluded_paths_not_excluded_file_relative(tmp_dir, create_random_fi
     xls_file = create_random_file(tmp_dir, suffix=".xls")
     xls_file_relative = xls_file.relative_to(tmp_dir.parent)
     paths = {csv_subfolder_file}
-    excluded_paths = DependencyPathManagement.get_excluded_paths(
+    excluded_paths = path_management.get_excluded_paths(
         src=[tmp_dir], excluded=[], not_excluded=[xls_file_relative], excluded_regex=[]
     )
     assert paths == excluded_paths
@@ -156,7 +153,7 @@ def test_get_excluded_paths_not_excluded_folder(tmp_dir, create_random_folder, c
     subfolder = create_random_folder(tmp_dir)
     create_random_file(subfolder, suffix=".xls")
     paths = set()
-    excluded_paths = DependencyPathManagement.get_excluded_paths(
+    excluded_paths = path_management.get_excluded_paths(
         src=[tmp_dir], excluded=[], not_excluded=[subfolder], excluded_regex=[]
     )
     assert paths == excluded_paths
@@ -166,9 +163,7 @@ def test_copy_file_absolute(src_dir, dest_dir, create_random_file, create_random
     file = create_random_file(src_dir)
     subfolder = create_random_folder(src_dir)
     subfolder_file = create_random_file(subfolder)
-    DependencyPathManagement.copy_paths(
-        src=[src_dir], dest_dir=dest_dir, excluded=[], excluded_regex=[], not_excluded=[]
-    )
+    path_management.copy_paths(src=[src_dir], dest_dir=dest_dir, excluded=[], excluded_regex=[], not_excluded=[])
 
     to_be_copied = [src_dir, file, subfolder, subfolder_file]
     to_be_copied_dest_dir = {dest_dir / f.relative_to(src_dir.parent) for f in to_be_copied}
@@ -185,9 +180,7 @@ def test_copy_file_relative(src_dir, dest_dir, create_random_file, create_random
     local_path = Path()
     local_src_file = src_dir.relative_to(local_path.absolute())
 
-    DependencyPathManagement.copy_paths(
-        src=[local_src_file], dest_dir=dest_dir, excluded=[], excluded_regex=[], not_excluded=[]
-    )
+    path_management.copy_paths(src=[local_src_file], dest_dir=dest_dir, excluded=[], excluded_regex=[], not_excluded=[])
 
     to_be_copied = [src_dir, file, subfolder, subfolder_file]
     to_be_copied_dest_dir = {dest_dir / f.relative_to(src_dir.parent) for f in to_be_copied}
