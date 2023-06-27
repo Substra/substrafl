0cbccaafef6b5d5aeec142d88b85f1fb2fc1dcfa
Guilhem Barthes <guilhem.barthes@owkin.com>
2023-06-27 15:17:49 +0200

    chore: remove `substrafl.tests.utils.wait`
    Signed-off-by: Guilhem Barthes <guilhem.barthes@owkin.com>

--- tests/utils.py
+++ tests/utils.py
@@ -1,74 +1,7 @@
 import pickle
-import time
-
-from substra.sdk.models import ComputePlanStatus
-from substra.sdk.models import Status
 
 from substrafl.nodes.node import OutputIdentifiers
 from substrafl.schemas import TaskType
-
-FUTURE_TIMEOUT = 3600
-FUTURE_POLLING_PERIOD = 1
-
-
-_get_methods = {
-    "Task": "get_task",
-    "ComputePlan": "get_compute_plan",
-}
-
-
-class TError(Exception):
-    """Substra Test Error."""
-
-    pass
-
-
-class FutureTimeoutError(TError):
-    """Future execution timed out."""
-
-    pass
-
-
-class FutureFailureError(TError):
-    """Future execution failed."""
-
-    pass
-
-
-def wait(client, asset, timeout=FUTURE_TIMEOUT, raises=True):
-    try:
-        m = _get_methods[asset.__class__.__name__]
-    except KeyError:
-        raise KeyError("Future not supported")
-    getter = getattr(client, m)
-
-    key = asset.key
-
-    tstart = time.time()
-    while asset.status not in [
-        Status.done.value,
-        Status.failed.value,
-        Status.canceled.value,
-        ComputePlanStatus.done.value,
-        ComputePlanStatus.failed.value,
-        ComputePlanStatus.canceled.value,
-    ]:
-        if time.time() - tstart > timeout:
-            raise FutureTimeoutError(f"Future timeout on {asset}")
-
-        time.sleep(FUTURE_POLLING_PERIOD)
-        asset = getter(key)
-
-    if raises and asset.status in (Status.failed.value, ComputePlanStatus.failed.value):
-        raise FutureFailureError(f"Future execution failed on {asset}")
-
-    if raises and asset.status in (
-        Status.canceled.value,
-        ComputePlanStatus.canceled.value,
-    ):
-        raise FutureFailureError(f"Future execution canceled on {asset}")
-
-    return asset
 
 
 def download_train_task_models_by_rank(network, session_dir, my_algo, compute_plan, rank: int):
