aae09a73864ccf3463a3a2c671eb90c2d1c51c33
jeandut <jean.du-terrail@owkin.com>
2023-08-28 14:56:20 +0200

    no deepcopy for anything but strategy
    Signed-off-by: jeandut <jean.du-terrail@owkin.com>

--- substrafl/experiment.py
+++ substrafl/experiment.py
@@ -278,11 +278,11 @@ def execute_experiment(
     if dependencies is None:
         dependencies = Dependency()
 
-    train_data_nodes = copy.deepcopy(train_data_nodes)
-    aggregation_node = copy.deepcopy(aggregation_node)
     strategy = copy.deepcopy(strategy)
 
     if not(isinstance(train_data_nodes[0], SimuTrainDataNode)):
+        train_data_nodes = copy.deepcopy(train_data_nodes)
+        aggregation_node = copy.deepcopy(aggregation_node)
         evaluation_strategy = copy.deepcopy(evaluation_strategy)
 
     train_organization_ids = [train_data_node.organization_id for train_data_node in train_data_nodes]
