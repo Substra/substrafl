46dcbe86569551c6a50322ed9468b918f96815f8
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-09-07 10:53:28 +0200

    chore: rebase
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -42,9 +42,9 @@ setup(
     install_requires=[
         "numpy>=1.20.3, <1.24",
         "cloudpickle>=1.6.0",
-        "substra~=0.47.0",
+        "substra~=0.47.0dev1",
         "substratools~=0.20.0",
-        "pydantic<3.0",
+        "pydantic>=2.3.0,<3.0",
         "pip>=21.2",
         "wheel",
         "six",
