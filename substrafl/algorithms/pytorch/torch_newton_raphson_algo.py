5f17053f5ad82d1a54c2a78c6ef7a9030e166e95
Mathieu Andreux <mathieu.andreux@owkin.com>
2024-03-07 17:58:05 +0100

    numpy syntax
    Signed-off-by: Mathieu Andreux <mathieu.andreux@owkin.com>

--- substrafl/algorithms/pytorch/torch_newton_raphson_algo.py
+++ substrafl/algorithms/pytorch/torch_newton_raphson_algo.py
@@ -408,7 +408,7 @@ class TorchNewtonRaphsonAlgo(TorchAlgo):
 
         hessian = self._hessian_shape(second_order_derivative)
 
-        hessian = 0.5 * hessian + 0.5 * hessian.t()  # ensure the hessian is symmetric
+        hessian = 0.5 * hessian + 0.5 * hessian.T  # ensure the hessian is symmetric
 
         return gradients, hessian
 
