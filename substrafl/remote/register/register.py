9c9e26747b87fd61c4aa7baa0a245816bdbfec44
Guilhem Barthés <guilhem.barthes@owkin.com>
2024-08-20 16:30:44 +0200

    chore: remove dev librairies after building to decrease CVE in nvidia image
    Signed-off-by: Guilhem Barthés <guilhem.barthes@owkin.com>

--- substrafl/remote/register/register.py
+++ substrafl/remote/register/register.py
@@ -73,6 +73,10 @@ COPY requirements.txt requirements.txt
 # Install requirements
 RUN python{python_version} -m pip install --no-cache-dir -r requirements.txt
 
+USER root
+RUN apt-get purge -y --auto-remove build-essential *-dev
+USER user
+
 # Copy all other files
 COPY function.py .
 COPY {internal_dir}/cls_cloudpickle {internal_dir}/
